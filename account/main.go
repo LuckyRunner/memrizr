d339b8d0665b7a63c5f3f816b45d35e698d29039
Duke <duke@DukedeMacBook-Air.local>
2024-04-14 15:34:25 +0800

    start account app with docker

---
+++ account/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	log.Println("Starting server...")
+
+	router := gin.Default()
+
+	router.GET("/api/account", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "suckas",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v", err)
+		}
+	}()
+
+	log.Printf("Listening on port %s\n", srv.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// This blocks until a signal is passed into the quit channel
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Failed to shutdown server: %v", err)
+	}
+
+	log.Println("Server shut down")
+}
